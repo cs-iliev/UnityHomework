7e908059a77f06b229b0341852b7ed875b9efd7b
cs-iliev <tsvetomir.iliev@singlet.dev>
2022-04-06 19:54:10 +0300

    Increase max key count

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ItemCollector : MonoBehaviour
 {
 
-    private const int MAX_KEYS = 3;
+    private const int MAX_KEYS = 10;
     [SerializeField] private GameObject[] keyBar = new GameObject[MAX_KEYS];
 
     private int collectedKeys = 0;
